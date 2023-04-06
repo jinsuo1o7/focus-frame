6bf7f1bfa07281300cd869d76d8b00fb5175ac95
Jinsuo <cil04110@gmail.com>
2023-04-06 15:52:05 +0900

    add heart, bookmarks icon - add react icon

---
+++ src/components/icons/BookMarkLine.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { BsBookmark } from "react-icons/bs";
+
+export default function BookMarkLine() {
+  return <BsBookmark />;
+}
---
+++ src/components/icons/HeartFill.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { AiFillHeart } from "react-icons/ai";
+
+export default function HeartFill() {
+  return <AiFillHeart />;
+}
