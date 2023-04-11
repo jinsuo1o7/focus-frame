9854ac9df7a22757e3589db960735a118033649e
Jinsuo <cil04110@gmail.com>
2023-04-11 12:50:22 +0900

    fix icon color - change heart, bookmark icon color

--- src/components/icons/HeartFill.tsx
+++ src/components/icons/HeartFill.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { AiFillHeart } from "react-icons/ai";
 
 export default function HeartFill() {
-  return <AiFillHeart />;
+  return <AiFillHeart className="text-red-400" />;
 }
--- src/components/icons/HeartLine.tsx
+++ src/components/icons/HeartLine.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { AiOutlineHeart } from "react-icons/ai";
 
 export default function HeartLine() {
-  return <AiOutlineHeart />;
+  return <AiOutlineHeart className="text-red-400" />;
 }
